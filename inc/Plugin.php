6453471b4a70192befa2f8826dab07d98703047f
badasswp <badasswpdev@gmail.com>
2024-07-04 13:39:34 +0100

    chore: on post creation, ping slack

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -49,4 +49,36 @@ class Plugin {
 
 		return static::$instance;
 	}
+
+	/**
+	 * Bind to WP.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function run(): void {
+		add_action( 'publish_post', [ $this, 'ping_on_post_creation' ], 10, 2 );
+	}
+
+	/**
+	 * Ping on Post Creation.
+	 *
+	 * Send notification to Slack channel when a
+	 * Post is created.
+	 *
+	 * @param int      $post_id Post ID.
+	 * @param \WP_Post $post    WP Post.
+	 *
+	 * @return void
+	 */
+	public function ping_on_post_creation( $post_id, $post ): void {
+		$message = sprintf(
+			'A Post was just created! ID: %s, Post Title: %s',
+			esc_html( $post_id ),
+			esc_html( get_post_field( 'post_title', $post_id ) )
+		);
+
+		$client->withIcon( ':ghost' )->send( $message );
+	}
 }
