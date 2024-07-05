791d51eafc6ccaaf15c453298798e21e87819230
badasswp <badasswpdev@gmail.com>
2024-07-05 10:06:35 +0100

    refactor: plugin class to use Container

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -10,8 +10,6 @@
 
 namespace PingMySlack;
 
-use Maknz\Slack\Client;
-
 class Plugin {
 	/**
 	 * Plugin instance.
@@ -21,17 +19,6 @@ class Plugin {
 	 * @var Plugin
 	 */
 	protected static $instance;
-
-	/**
-	 * Set up.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return void
-	 */
-	public function __construct() {
-		$this->client = new Client();
-	}
 
 	/**
 	 * Get Instance.
@@ -58,27 +45,6 @@ class Plugin {
 	 * @return void
 	 */
 	public function run(): void {
-		add_action( 'publish_post', [ $this, 'ping_on_post_creation' ], 10, 2 );
-	}
-
-	/**
-	 * Ping on Post Creation.
-	 *
-	 * Send notification to Slack channel when a
-	 * Post is created.
-	 *
-	 * @param int      $post_id Post ID.
-	 * @param \WP_Post $post    WP Post.
-	 *
-	 * @return void
-	 */
-	public function ping_on_post_creation( $post_id, $post ): void {
-		$message = sprintf(
-			'A Post was just created! ID: %s, Post Title: %s',
-			esc_html( $post_id ),
-			esc_html( get_post_field( 'post_title', $post_id ) )
-		);
-
-		$client->withIcon( ':ghost' )->send( $message );
+		( new Container() )->register();
 	}
 }
