d234ae64a61ac941bc3cbd4c0e2b263b268018be
badasswp <badasswpdev@gmail.com>
2024-07-07 09:19:09 +0100

    feat: start logging user and date

--- inc/User.php
+++ inc/User.php
@@ -38,12 +38,14 @@ class User extends Service {
 	 */
 	public function ping_on_user_login( $user_login, $user ): void {
 		$message = sprintf(
-			"Ping: %s \n%s: %s, \n%s: %s",
+			"Ping: %s \n%s: %s, \n%s: %s, \n%s: %s",
 			esc_html__( 'A User just logged in!', 'ping-my-slack' ),
 			esc_html__( 'ID', 'ping-my-slack' ),
 			esc_html( $user->ID ),
 			esc_html__( 'User', 'ping-my-slack' ),
-			esc_html( $user_login )
+			esc_html( $user_login ),
+			esc_html__( 'Date', 'ping-my-slack' ),
+			esc_html( date( 'H:i:s, d-m-Y' ) )
 		);
 
 		/**
@@ -62,5 +64,9 @@ class User extends Service {
 		$message = apply_filters( "ping_my_slack_login_message", $message, $user );
 
 		$this->client->ping( $message );
+	}
+
+	public function ping_on_user_logout(): void {
+
 	}
 }
