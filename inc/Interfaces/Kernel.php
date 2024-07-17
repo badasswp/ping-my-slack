0f79d12ebd6a997ed62d5c38d00b4a84fb952756
badasswp <badasswpdev@gmail.com>
2024-07-17 15:36:49 +0100

    feat: implement Kernel interface

---
+++ inc/Interfaces/Kernel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Kernel Interface.
+ *
+ * Define the base methods that must be shared
+ * across concerte classes.
+ *
+ * @package PingMySlack
+ */
+
+interface Kernel {
+	/**
+	 * Register call.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register(): void;
+}
