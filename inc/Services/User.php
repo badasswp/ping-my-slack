c5bdf346431142c1bdcc2c9565539183dfc81afc
badasswp <badasswpdev@gmail.com>
2024-07-19 18:49:46 +0100

    feat: implement user modification method

--- inc/Services/User.php
+++ inc/Services/User.php
@@ -23,6 +23,7 @@ class User extends Service implements Kernel {
 	 */
 	public function register(): void {
 		add_action( 'user_register', [ $this, 'ping_on_user_creation' ], 10, 2 );
+		add_action( 'wp_update_user', [ $this, 'ping_on_user_modification' ], 10, 3 );
 		add_action( 'deleted_user', [ $this, 'ping_on_user_deletion' ], 10, 3 );
 	}
 
@@ -65,6 +66,46 @@ class User extends Service implements Kernel {
 		 * @return string
 		 */
 		$message = apply_filters( 'ping_my_slack_user_creation_message', $message, $user_id );
+
+		$this->client->ping( $message );
+	}
+
+	/**
+	 * Ping on User Modification.
+	 *
+	 * This method sends event logging to the Slack Workspace
+	 * on user modification/update.
+	 *
+	 * @param int   $user_id      The ID of the user that was just updated.
+	 * @param array $userdata     The array of user data that was updated.
+	 * @param array $userdata_raw The unedited array of user data that was updated.
+	 */
+	public function ping_on_user_modification( $user_id, $userdata, $userdata_raw ) {
+		$message = sprintf(
+			"Ping: %s \n%s: %s \n%s: %s \n%s: %s",
+			esc_html__( 'A User was just modified!', 'ping-my-slack' ),
+			esc_html__( 'ID', 'ping-my-slack' ),
+			esc_html( $user_id ),
+			esc_html__( 'User', 'ping-my-slack' ),
+			esc_html( get_user_by( 'id', $user_id )->user_login ),
+			esc_html__( 'Date', 'ping-my-slack' ),
+			esc_html( gmdate( 'H:i:s, d-m-Y' ) )
+		);
+
+		/**
+		 * Filter Ping Message.
+		 *
+		 * Set custom Slack message to be sent when a WP
+		 * user is modified or updated.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param string   $message Slack Message.
+		 * @param int      $user_id User ID.
+		 *
+		 * @return string
+		 */
+		$message = apply_filters( 'ping_my_slack_user_modification_message', $message, $user_id );
 
 		$this->client->ping( $message );
 	}
