a9e926e5173a2216b7e86fd4ed3c0279bede504b
badasswp <badasswpdev@gmail.com>
2024-07-21 14:06:41 +0100

    test: add new test for ping on comment status change

--- tests/Services/CommentTest.php
+++ tests/Services/CommentTest.php
@@ -10,6 +10,7 @@ use PingMySlack\Services\Comment;
 /**
  * @covers \PingMySlack\Services\Comment::__construct
  * @covers \PingMySlack\Services\Comment::register
+ * @covers \PingMySlack\Services\Comment::ping_on_comment_status_change
  */
 class CommentTest extends TestCase {
 	public function setUp(): void {
@@ -31,6 +32,15 @@ class CommentTest extends TestCase {
 		\WP_Mock::expectActionAdded( 'transition_comment_status', [ $this->comment, 'ping_on_comment_status_change' ], 10, 3 );
 
 		$this->comment->register();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_ping_on_comment_status_change_bails() {
+		$comment = Mockery::mock( \WP_Comment::class )->makePartial();
+		$comment->shouldAllowMockingProtectedMethods();
+
+		$this->comment->ping_on_comment_status_change( 'approved', 'approved', $comment );
 
 		$this->assertConditionsMet();
 	}
