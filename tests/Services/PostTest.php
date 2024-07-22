f561ec1685fb1b564278968d851d732fce10321f
badasswp <badasswpdev@gmail.com>
2024-07-22 10:49:42 +0100

    test: add tests for Post register method

---
+++ tests/Services/PostTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PingMySlack\Tests\Services;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+use PingMySlack\Services\Post;
+
+/**
+ * @covers \PingMySlack\Services\Post::__construct
+ * @covers \PingMySlack\Services\Post::register
+ */
+class PostTest extends TestCase {
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		$this->post = Mockery::mock( Post::class )->makePartial();
+		$this->post->shouldAllowMockingProtectedMethods();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_register() {
+		\WP_Mock::expectActionAdded( 'transition_post_status', [ $this->post, 'ping_on_post_status_change' ], 10, 3 );
+
+		$this->post->register();
+
+		$this->assertConditionsMet();
+	}
+}
