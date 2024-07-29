95e0a7b72c6c0f0eb2165de1349182164f89b7d6
badasswp <badasswpdev@gmail.com>
2024-07-29 23:27:12 +0100

    test: add set up test for Client class

---
+++ tests/Core/ClientTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PingMySlack\Tests\Core;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+use PingMySlack\Core\Client;
+
+/**
+ * @covers \PingMySlack\Core\Client::__construct
+ */
+class ClientTest extends TestCase {
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		\WP_Mock::userFunction( 'get_option' )
+			->once()
+			->with( 'ping_my_slack', [] )
+			->andReturn(
+				[
+					'channel'  => '#general',
+					'username' => 'Bryan',
+				]
+			);
+
+		$this->client = new Client();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_args_set_up_default() {
+		$this->assertSame(
+			'#general',
+			$this->client->args['channel']
+		);
+		$this->assertSame(
+			'Bryan',
+			$this->client->args['username']
+		);
+		$this->assertConditionsMet();
+	}
+}
