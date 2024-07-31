bf298a79a0ab5c52fbabe600b29cd7f5a5ca839a
badasswp <badasswpdev@gmail.com>
2024-07-31 06:45:32 +0100

    fix: use get date method

--- inc/Services/Access.php
+++ inc/Services/Access.php
@@ -62,7 +62,7 @@ class Access extends Service implements Kernel {
 			esc_html__( 'User', 'ping-my-slack' ),
 			esc_html( $user_login ),
 			esc_html__( 'Date', 'ping-my-slack' ),
-			esc_html( gmdate( 'H:i:s, d-m-Y' ) )
+			esc_html( $this->get_date() )
 		);
 
 		/**
@@ -116,7 +116,7 @@ class Access extends Service implements Kernel {
 			esc_html__( 'User', 'ping-my-slack' ),
 			esc_html( get_user_by( 'id', $user_id )->user_login ),
 			esc_html__( 'Date', 'ping-my-slack' ),
-			esc_html( gmdate( 'H:i:s, d-m-Y' ) )
+			esc_html( $this->get_date() )
 		);
 
 		/**
