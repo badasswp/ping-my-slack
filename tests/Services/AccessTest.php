21bacbd70434422a134b33c88257734c375a5cd6
badasswp <badasswpdev@gmail.com>
2024-07-31 06:49:52 +0100

    test: add test for ping on user login

--- tests/Services/AccessTest.php
+++ tests/Services/AccessTest.php
@@ -35,4 +35,54 @@ class AccessTest extends TestCase {
 
 		$this->assertConditionsMet();
 	}
+
+	public function test_ping_on_user_login() {
+		$user_login = 'john@doe.com';
+
+		$user     = Mockery::mock( \WP_User::class )->makePartial();
+		$user->ID = 1;
+
+		\WP_Mock::expectFilter( 'ping_my_slack_login_client', $this->access->client );
+
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 5,
+				'return' => function ( $text, $domain = 'ping-my-slack' ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_html',
+			[
+				'times'  => 3,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		$message = "Ping: A User just logged in! \nID: 1 \nUser: john@doe.com \nDate: 08:57:13, 01-07-2024";
+
+		\WP_Mock::expectFilter(
+			'ping_my_slack_login_message',
+			$message,
+			$user
+		);
+
+		$this->access->shouldReceive( 'get_date' )
+			->once()
+			->with()
+			->andReturn( '08:57:13, 01-07-2024' );
+
+		$this->access->client->shouldReceive( 'ping' )
+			->once()
+			->with( $message );
+
+		$this->access->ping_on_user_login( $user_login, $user );
+
+		$this->assertConditionsMet();
+	}
 }
