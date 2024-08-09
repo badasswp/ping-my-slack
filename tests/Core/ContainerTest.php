6ba034493885338d5dd853a016e9f87e9067c20d
badasswp <badasswpdev@gmail.com>
2024-08-09 09:22:16 +0100

    chore: remove faulty test

--- tests/Core/ContainerTest.php
+++ tests/Core/ContainerTest.php
@@ -40,32 +40,4 @@ class ContainerTest extends TestCase {
 		$this->assertTrue( in_array( User::class, Container::$services, true ) );
 		$this->assertConditionsMet();
 	}
-
-	/*public function test_register() {
-		$this->services = [
-			'access'  => Access::get_instance(),
-			'admin'   => Admin::get_instance(),
-			'boot'    => Boot::get_instance(),
-			'comment' => Comment::get_instance(),
-			'post'    => Post::get_instance(),
-			'theme'   => Theme::get_instance(),
-			'user'    => User::get_instance(),
-		];
-
-		\WP_Mock::expectActionAdded( 'init', [ Service::$services['PingMySlack\Services\Boot'], 'ping_my_slack_translation' ] );
-		\WP_Mock::expectActionAdded( 'wp_login', [ Service::$services['PingMySlack\Services\Access'], 'ping_on_user_login' ], 10, 2 );
-		\WP_Mock::expectActionAdded( 'wp_logout', [ Service::$services['PingMySlack\Services\Access'], 'ping_on_user_logout' ] );
-		\WP_Mock::expectActionAdded( 'plugins_loaded', [ Service::$services['PingMySlack\Services\Admin'], 'carbon_fields_init' ] );
-		\WP_Mock::expectActionAdded( 'carbon_fields_register_fields', [ Service::$services['PingMySlack\Services\Admin'], 'get_admin_page' ] );
-		\WP_Mock::expectActionAdded( 'transition_comment_status', [ Service::$services['PingMySlack\Services\Comment'], 'ping_on_comment_status_change' ], 10, 3 );
-		\WP_Mock::expectActionAdded( 'transition_post_status', [ Service::$services['PingMySlack\Services\Post'], 'ping_on_post_status_change' ], 10, 3 );
-		\WP_Mock::expectActionAdded( 'switch_theme', [ Service::$services['PingMySlack\Services\Theme'], 'ping_on_theme_change' ], 10, 3 );
-		\WP_Mock::expectActionAdded( 'user_register', [ Service::$services['PingMySlack\Services\User'], 'ping_on_user_creation' ], 10, 2 );
-		\WP_Mock::expectActionAdded( 'wp_update_user', [ Service::$services['PingMySlack\Services\User'], 'ping_on_user_modification' ], 10, 3 );
-		\WP_Mock::expectActionAdded( 'deleted_user', [ Service::$services['PingMySlack\Services\User'], 'ping_on_user_deletion' ], 10, 3 );
-
-		$this->container->register();
-
-		$this->assertConditionsMet();
-	}*/
 }
