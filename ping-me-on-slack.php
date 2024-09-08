d174dd7abadf663a5516539483edbcb42c50bf97
badasswp <badasswpdev@gmail.com>
2024-09-08 15:49:59 +0100

    chore: modify name to ping me on slack

--- ping-me-on-slack.php
+++ ping-me-on-slack.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * Plugin Name: Ping My Slack
- * Plugin URI:  https://github.com/badasswp/ping-my-slack
+ * Plugin Name: Ping Me On Slack
+ * Plugin URI:  https://github.com/badasswp/ping-me-on-slack
  * Description: Get notifications on Slack when changes are made on your WP website.
  * Version:     1.0.0
  * Author:      badasswp
  * Author URI:  https://github.com/badasswp
  * License:     GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain: ping-my-slack
+ * Text Domain: ping-me-on-slack
  * Domain Path: /languages
  *
- * @package PingMySlack
+ * @package PingMeOnSlack
  */
 
-namespace badasswp\PingMySlack;
+namespace badasswp\PingMeOnSlack;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	wp_die();
@@ -23,10 +23,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once __DIR__ . '/init.php';
 
 // Bail out, if Composer is NOT installed.
-if ( ! file_exists( PINGMYSLACK ) ) {
-	add_action( 'admin_notices', 'ping_my_slack_notice' );
+if ( ! file_exists( PINGMEONSLACK ) ) {
+	add_action( 'admin_notices', 'ping_me_on_slack_notice' );
 	return;
 }
 
 // Run Plugin.
-ping_my_slack_run( PINGMYSLACK );
+ping_me_on_slack_run( PINGMYSLACK );
